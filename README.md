d41ab03a7488fe1df90e749c78bec9e9c9157196
sicopompo <pabalalop@gmail.com>
2023-08-24 17:04:17 +0200

    First commit of the guide

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # single-gpu-passthrough-3060-b450plus-ryzen5600g
-Steps to do one single gpu passthrough from host to virtual machine (guest) for windows as guesst
+Steps to do one single gpu passthrough from host to virtual machine (guest) for windows as guest with a endeavourOS kvm host
+
+Sources:
+
+https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF
+
+https://github.com/joeknock90/Single-GPU-Passthrough
+
---
+++ scripts/IOMMU-group.sh
@@ -0,0 +1,15 @@
+
+pt -s nullglob
+for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
+    echo "IOMMU Group ${g##*/}:"
+    for d in $g/devices/*; do
+        echo -e "\t$(lspci -nns ${d##*/})"
+    done;
+done;#!/bin/bash
+shopt -s nullglob
+for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
+    echo "IOMMU Group ${g##*/}:"
+    for d in $g/devices/*; do
+        echo -e "\t$(lspci -nns ${d##*/})"
+    done;
+done;
